a3ea1b3b56522e5a6973d050214d5672ddec0f0a
KOMMANISURESH1234 <sathyasuresh358@gmail.com>
2023-11-21 11:19:26 +0530

    WELCOME CLASS IS UPDATED

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -2,6 +2,7 @@ package com.edubridge.app1;
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO JAVAA");
+		System.out.println("WELCOME TO PYTHON");
 	}
 
 }
